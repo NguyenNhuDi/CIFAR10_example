0a1f6071ad14be48730b9d11a34105ba895dc80f
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-10-11 17:00:41 -0600

    re made train val splits

--- Train/CIFAR10_trainer.py
+++ Train/CIFAR10_trainer.py
@@ -8,11 +8,14 @@ import matplotlib.pyplot as plt
 import yaml
 import albumentations as A
 
-csv_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\CSV\0.csv'
-yml_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\YAMLS\train_labels.yml'
-image_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\cifar-10\train'
+
+csv_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\TRAIN_VAL\0.csv'
+yml_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\TRAIN_VAL\train_labels.yml'
+train_image_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\cifar-10\train'
+test_image_path = r'C:\Users\coanh\Desktop\UNI\AIC\Classification Competetion\cifar10\test'
 
 if __name__ == '__main__':
+
     class CIFARDataset(Dataset):
         def __init__(self, csv_dir, yml_labels, image_dir, transform: A.Compose = None, train=True):
             data = pd.read_csv(csv_dir)
@@ -44,7 +47,7 @@ if __name__ == '__main__':
     with open(yml_path, 'r') as f:
         yml_label = yaml.safe_load(f)
 
-    testSet = CIFARDataset(yml_labels=yml_label, image_dir=image_path, csv_dir=csv_path)
+    testSet = CIFARDataset(yml_labels=yml_label, image_dir=train_image_path, csv_dir=csv_path)
 
     for i in range(3):
         image, label = testSet.__getitem__(i)
